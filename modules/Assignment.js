6ea55a9595dd053b1df3c6c8b799ad7491c91997
Kang Lu <kang.lu@pvelocity.com>
2023-04-01 10:43:55 -0400

    Added documentation to the Class Index.

--- modules/Assignment.js
+++ modules/Assignment.js
@@ -28,7 +28,7 @@ class Index {
    * non-destructive merge sort. The sorted results can be
    * retrieved, and searched by its member functions.
    *
-   * @param {[String]} arr The input array to be sorted.
+   * @param {String} arr The input array to be sorted.
    */
   constructor(arr) {
     if (!Array.isArray(arr)) {
@@ -53,6 +53,21 @@ class Index {
     this.sort();
   }
 
+  /**
+   * Merge between two previously sorted regions by the same
+   * function.
+   *
+   * @param {Number} b  An index pointing to the beginning of
+   *                    the first segment.
+   *
+   * @param {Number} m  An index pointing to the middle element
+   *                    that separates between the first and the
+   *                    second segments. This middle element is
+   *                    inclusively belonging to the first segment.
+   *
+   * @param {Number} e  An index that is 'one beyond' the last index
+   *                    of the second segment.
+   */
   merge(b, m, e) {
     let i = 0;
     let j = 0;
@@ -84,6 +99,18 @@ class Index {
     }
   }
 
+  /**
+   * Initiate the merge sort of the entire array that was given when
+   * this object was constructed.
+   *
+   * Note that we performed two major optimizations here. The first is
+   * to eliminate recursive calls by first predetermine all the calls
+   * needed to perform a merge. The second is to perform manual sorting
+   * for segments that are lengths of 2, 3, and 4 instead of going
+   * through the generic merge.
+   *
+   * Depending on certain scenario, it can beat the built-in sort.
+   */
   sort() {
     let segReqProcB = [0];
     let segReqProcE = [this.arr.length];
@@ -184,6 +211,16 @@ class Index {
     );
   }
 
+  /**
+   * For a given search term, get the range that exactly matches the term.
+   *
+   * Note that the first element is inclusive, while the second element
+   * of the range is "not-inclusive".
+   *
+   * @param {String} term The string that we will use to search the index.
+   *
+   * @return {Array} [from, to] where to is not inclusive.
+   */
   getRangeOf(term) {
     let to = -1;
     let from = this.getFirstIndexOf(term);
@@ -202,6 +239,14 @@ class Index {
     }
   }
 
+  /**
+   * Obtain the first index of the search term.
+   *
+   * @param {String} term The string that we will use to search the index.
+   *
+   * @return {Number} an index to the index array that exactly matches
+   *                  the search term.
+   */
   getFirstIndexOf(term) {
     let min = 0;
     let max = this.arr.length;
@@ -244,6 +289,13 @@ class Index {
     return r;
   }
 
+  /**
+   * Obtain the nth value of the sorted index.
+   *
+   * @param {Number} idx The index to the sorted index.
+   *
+   * @return {String} The value at idx.
+   */
   nth(idx) {
     if (Number.isInteger(idx) && idx >= 0 && idx <= this.iarr.length) {
       return this.arr[this.iarr[idx]];
@@ -251,6 +303,22 @@ class Index {
     return "";
   }
 
+  /**
+   * Return an array of sorted index values of a given range.
+   *
+   * @param  {Object} from  This can either be a number or an array. If
+   *                        it is a number, then it points to the start
+   *                        of the range. If it is an array, then it
+   *                        must contains two elements, each representing
+   *                        the start, and one beyond the end of the range.
+   *
+   * @param {Number} to   This is an integer that points to the element
+   *                      "one past" the last element to be retrieved.
+   *                      This element is ignored if the "from" parameter
+   *                      is an array.
+   *
+   * @return An array of values from the index range.
+   */
   rangeValues(from, to) {
     let start = from;
     let end = to;
